6afad17c6a5d16fb718a48ce108d0a5d752496b1
jbneto <jbneto.ep@gmail.com>
2022-11-02 16:10:13 -0300

    adicionei teste de operadores logicos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,17 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        //String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //int numero = 5;
+       // numero++;
+       // System.out.println(numero);
+
+       int a, b;
+
+        a = 5;
+        b = 6;
+
+    String resultado = (a==b) ? "verdadeiro" : "false";
+    System.out.println(resultado);
+
+
+}}
---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,21 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        String meuNome = "Neto";
+        
+        double salarioMinimo = 1212;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        int numeroCurto2 = numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        
+    }
+}
